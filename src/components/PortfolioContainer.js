a3ece5e4806c1e5516a7b2f736810516763672c9
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-11-08 11:45:09 -0600

    function to render diff component on change

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -6,6 +6,21 @@ import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 
 export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('AboutMe');
+
+    const renderPage = function() {
+        switch (currentPage) {
+            case 'AboutMe':
+                return <AboutMe />;
+            case 'Portfolio':
+                return <Portfolio />;
+            case 'Contact':
+                return <Contact />;
+            default:
+                return <Resume />;
+        };
+    };
+
     return (
         <div>
             <Nav />
