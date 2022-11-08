b53946bed4ce12eaa81eef928285032dd78ad586
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-11-08 11:56:20 -0600

    clickinga nav link changes the style of it

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,10 +3,26 @@ import React from 'react';
 export default function Nav({ currentPage, handlePageChange }) {
     return (
         <ul>
-            <li>About Me</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
-            <li>Resume</li>
+            <li>
+                <a href='#' onClick={handlePageChange('AboutMe')} className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}>
+                    About Me
+                </a>
+            </li>
+            <li>
+                <a href='#' onClick={handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+                    Portfolio
+                </a>
+            </li>
+            <li>
+                <a href='#' onClick={handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                    Contact
+                </a>
+            </li>
+            <li>
+                <a href='#' onClick={handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
+                    Resume
+                </a>
+            </li>
         </ul>
     )
 }
