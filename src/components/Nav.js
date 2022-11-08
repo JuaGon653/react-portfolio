c1e561477ec623c3be0e08a398f890f04d915538
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-11-08 12:03:23 -0600

    clinking tab changes appearance

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,24 +2,24 @@ import React from 'react';
 
 export default function Nav({ currentPage, handlePageChange }) {
     return (
-        <ul>
+        <ul className='nav'>
             <li>
-                <a href='#' onClick={handlePageChange('AboutMe')} className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}>
+                <a href='#AboutMe' onClick={() => handlePageChange('AboutMe')} className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}>
                     About Me
                 </a>
             </li>
             <li>
-                <a href='#' onClick={handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+                <a href='#Porfolio' onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
                     Portfolio
                 </a>
             </li>
             <li>
-                <a href='#' onClick={handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                <a href='#Contact' onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
                     Contact
                 </a>
             </li>
             <li>
-                <a href='#' onClick={handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
+                <a href='#Resume' onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
                     Resume
                 </a>
             </li>
