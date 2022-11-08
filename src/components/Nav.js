674c785f9c972a19befe9714fc0f3caaf7a47919
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-11-08 11:40:18 -0600

    simple nav

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Nav({ currentPage, handlePageChange }) {
+    return (
+        <ul>
+            <li>About Me</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+            <li>Resume</li>
+        </ul>
+    )
+}
