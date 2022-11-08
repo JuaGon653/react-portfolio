029ad4f2f7312bffc76be0217d61d26a5243d4dd
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-11-08 11:34:45 -0600

    exports a div containing just the nav

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -4,3 +4,11 @@ import AboutMe from './pages/AboutMe';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+    return (
+        <div>
+            <Nav />
+        </div>
+    )
+}
