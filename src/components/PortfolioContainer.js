1f0957753ce03bc2bf90215f1eda885e7a81f9e9
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-11-13 16:00:50 -0600

    adding portfolio

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Nav from './Nav';
 import AboutMe from './pages/AboutMe';
-// import Portfolio from './pages/Portfolio';
+import Portfolio from './pages/Portfolio';
 // import Contact from './pages/Contact';
 // import Resume from './pages/Resume';
 import Footer from './Footer';
@@ -13,8 +13,8 @@ export default function PortfolioContainer() {
         switch (currentPage) {
             case 'AboutMe':
                 return <AboutMe />;
-            // case 'Portfolio':
-            //     return <Portfolio />;
+            case 'Portfolio':
+                return <Portfolio />;
             // case 'Contact':
             //     return <Contact />;
             // default:
