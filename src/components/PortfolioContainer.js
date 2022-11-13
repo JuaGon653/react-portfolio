6529b484d5c61bb448442d6632089f412e030e2c
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-11-13 16:07:21 -0600

    added contact page

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Nav from './Nav';
 import AboutMe from './pages/AboutMe';
 import Portfolio from './pages/Portfolio';
-// import Contact from './pages/Contact';
+import Contact from './pages/Contact';
 // import Resume from './pages/Resume';
 import Footer from './Footer';
 
@@ -15,8 +15,8 @@ export default function PortfolioContainer() {
                 return <AboutMe />;
             case 'Portfolio':
                 return <Portfolio />;
-            // case 'Contact':
-            //     return <Contact />;
+            case 'Contact':
+                return <Contact />;
             // default:
             //     return <Resume />;
         };
